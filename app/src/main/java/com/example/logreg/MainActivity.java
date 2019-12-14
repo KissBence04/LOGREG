be690082fd9f69bea2cd1714fb75eb9f563a8bde
Kiss Bence <kissbence2000@gmail.com>
2019-12-14 11:58:37 +0100

    Regisztrációra átugrás

--- app/src/main/java/com/example/logreg/MainActivity.java
+++ app/src/main/java/com/example/logreg/MainActivity.java
@@ -43,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
     public void init()
